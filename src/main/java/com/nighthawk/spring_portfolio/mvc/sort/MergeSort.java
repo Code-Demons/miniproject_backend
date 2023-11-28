65c334489dc2fb948735c2258c428df2414b84d6
Your Name <ryanrob327@gmail.com>
2023-11-28 09:42:16 -0800

    finished merge sort algorithm

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/sort/MergeSort.java
@@ -0,0 +1,51 @@
+// MergeSort.java
+public class MergeSort implements SortingAlgorithm {
+    @Override
+    public void sort(int[] array) {
+        mergeSort(array, 0, array.length - 1);
+    }
+
+    private void mergeSort(int[] array, int l, int r) {
+        if (l < r) {
+            int m = l + (r - l) / 2;
+            mergeSort(array, l, m);
+            mergeSort(array, m + 1, r);
+            merge(array, l, m, r);
+        }
+    }
+
+    private void merge(int[] array, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        System.arraycopy(array, l, L, 0, n1);
+        System.arraycopy(array, m + 1, R, 0, n2);
+
+        int i = 0, j = 0, k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                array[k] = L[i];
+                i++;
+            } else {
+                array[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            array[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            array[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+}
