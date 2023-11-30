2e181404be0ee5b3f2885b18120f18681e5a8ea5
Your Name <ryanrob327@gmail.com>
2023-11-30 15:42:55 -0800

    race returns an array of all data

--- src/main/java/com/nighthawk/spring_portfolio/mvc/sort/Race.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/sort/Race.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Arrays;
 
 
 public class Race {
@@ -12,7 +13,9 @@ public class Race {
 
         return array;
     }
-    public static void main(String[] args) {
+    public static long[] runSortingAlgorithms() {
+        long[] sortingTimes = new long[4];
+
         int arrayLength = 1000; // Adjust the length as needed
         int maxValue = 100; // Adjust the maximum value as needed
 
@@ -26,6 +29,7 @@ public class Race {
         bubbleSort.sort(array.clone());  // Clone to avoid modifying the original array
         long endTime = System.currentTimeMillis();
         long bubbleSortTime = endTime - startTime;
+        sortingTimes[0] = bubbleSortTime;
 
         // Run and measure Insertion Sort
         startTime = System.currentTimeMillis();
@@ -33,6 +37,7 @@ public class Race {
         insertionSort.sort(array.clone());
         endTime = System.currentTimeMillis();
         long insertionSortTime = endTime - startTime;
+        sortingTimes[1] = insertionSortTime;
 
         // Run and measure Merge Sort
         startTime = System.currentTimeMillis();
@@ -40,6 +45,7 @@ public class Race {
         mergeSort.sort(array.clone());
         endTime = System.currentTimeMillis();
         long mergeSortTime = endTime - startTime;
+        sortingTimes[2] = mergeSortTime;
 
         // Run and measure Quick Sort
         startTime = System.currentTimeMillis();
@@ -47,11 +53,24 @@ public class Race {
         quickSort.sort(array.clone());
         endTime = System.currentTimeMillis();
         long quickSortTime = endTime - startTime;
+        sortingTimes[3] = quickSortTime;
 
         // Print or store the results
         System.out.println("Bubble Sort Time: " + bubbleSortTime + " milliseconds");
         System.out.println("Insertion Sort Time: " + insertionSortTime + " milliseconds");
         System.out.println("Merge Sort Time: " + mergeSortTime + " milliseconds");
         System.out.println("Quick Sort Time: " + quickSortTime + " milliseconds");
+
+        // Return array of stats
+        // order of array: (bubble, insertion, merge, quick)
+        return sortingTimes;
+
+    }
+    public static void main(String[] args) {
+        // Adjust the length and constants accordingly
+        long[] sortingTimes = runSortingAlgorithms();
+
+        // Print or use the sorting times array as needed
+        System.out.println("Sorting Algorithm Times: " + Arrays.toString(sortingTimes));
     }
 }
