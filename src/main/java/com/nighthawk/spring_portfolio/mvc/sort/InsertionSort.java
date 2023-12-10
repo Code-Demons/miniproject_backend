ff84567222bf792bcdfe416d929dd67c06093aa6
nikhilc3 <nikhilchakravarthula14@gmail.com>
2023-12-09 16:36:19 -0800

    commitChange

--- src/main/java/com/nighthawk/spring_portfolio/mvc/sort/InsertionSort.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/sort/InsertionSort.java
@@ -1,17 +1,1 @@
 package com.nighthawk.spring_portfolio.mvc.sort;
-public class InsertionSort implements SortingAlgorithm {
-    @Override
-    public void sort(int[] array) {
-        int n = array.length;
-        for (int i = 1; i < n; ++i) {
-            int key = array[i];
-            int j = i - 1;
-
-            while (j >= 0 && array[j] > key) {
-                array[j + 1] = array[j];
-                j = j - 1;
-            }
-            array[j + 1] = key;
-        }
-    }
-}
--- src/main/java/com/nighthawk/spring_portfolio/mvc/sort/SortingAlgorithm.java
+++
@@ -1,5 +0,0 @@
-package com.nighthawk.spring_portfolio.mvc.sort;
-
-public interface SortingAlgorithm {
-    void sort(int[] array);
-}
