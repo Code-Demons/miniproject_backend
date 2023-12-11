42a5aa036b1fd3065bb95ef0e139c171903a622a
nikhilc3 <nikhilchakravarthula14@gmail.com>
2023-12-11 00:09:31 -0800

    FiboFinished!

--- src/main/java/com/nighthawk/spring_portfolio/controllers/FibonacciController.java
+++ src/main/java/com/nighthawk/spring_portfolio/controllers/FibonacciController.java
@@ -9,40 +9,46 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @CrossOrigin(origins = "http://127.0.0.1:4100")
 public class FibonacciController {
 
     @GetMapping("/fibonacci/forloop/{n}")
-    public String fibonacciForLoop(@PathVariable int n) {
-        long startTime = System.nanoTime();
-        long result = fibofor.fibonacci(n);
-        long endTime = System.nanoTime();
-        return "Result: " + result + ", Time taken: " + (endTime - startTime) + " nanoseconds";
+    public Map<String, Object> fibonacciForLoop(@PathVariable int n) {
+        return calculateFibonacci(n, fibofor::fibonacci);
     }
 
     @GetMapping("/fibonacci/recursion/{n}")
-    public String fibonacciRecursion(@PathVariable int n) {
-        long startTime = System.nanoTime();
-        long result = fiboRecursive.fibonacci(n);
-        long endTime = System.nanoTime();
-        return "Result: " + result + ", Time taken: " + (endTime - startTime) + " nanoseconds";
+    public Map<String, Object> fibonacciRecursion(@PathVariable int n) {
+        return calculateFibonacci(n, fiboRecursive::fibonacci);
     }
 
     @GetMapping("/fibonacci/whileloop/{n}")
-    public String fibonacciWhileLoop(@PathVariable int n) {
-        long startTime = System.nanoTime();
-        long result = fiboWhile.fibonacci(n);
-        long endTime = System.nanoTime();
-        return "Result: " + result + ", Time taken: " + (endTime - startTime) + " nanoseconds";
+    public Map<String, Object> fibonacciWhileLoop(@PathVariable int n) {
+        return calculateFibonacci(n, fiboWhile::fibonacci);
     }
 
     @GetMapping("/fibonacci/dynamic/{n}")
-    public String fibonacciDynamic(@PathVariable int n) {
+    public Map<String, Object> fibonacciDynamic(@PathVariable int n) {
+        return calculateFibonacci(n, fiboDynamic::fibonacci);
+    }
+
+    private Map<String, Object> calculateFibonacci(int n, FibonacciFunction fiboFunction) {
         long startTime = System.nanoTime();
-        long result = fiboDynamic.fibonacci(n);
+        long result = fiboFunction.fibonacci(n);
         long endTime = System.nanoTime();
-        return "Result: " + result + ", Time taken: " + (endTime - startTime) + " nanoseconds";
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("result", result);
+        response.put("timeTaken", endTime - startTime);
+        return response;
+    }
+
+    @FunctionalInterface
+    interface FibonacciFunction {
+        long fibonacci(int n);
     }
 }
