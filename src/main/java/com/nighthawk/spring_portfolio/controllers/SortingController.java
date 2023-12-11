76bc1c13834124d9a6af1a88e236734708183d59
nikhilc3 <nikhilchakravarthula14@gmail.com>
2023-12-10 23:59:26 -0800

    SortingDone!

--- src/main/java/com/nighthawk/spring_portfolio/controllers/SortingController.java
+++ src/main/java/com/nighthawk/spring_portfolio/controllers/SortingController.java
@@ -5,43 +5,46 @@ import com.nighthawk.spring_portfolio.mvc.sort.InsertionSort;
 import com.nighthawk.spring_portfolio.mvc.sort.MergeSort;
 import com.nighthawk.spring_portfolio.mvc.sort.SelectionSort;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://127.0.0.1:4100")
 public class SortingController {
 
     @GetMapping("/sort/bubble/{array}")
-    public String bubbleSort(@PathVariable int[] array) {
-        long startTime = System.nanoTime();
-        BubbleSort.sort(array);
-        long endTime = System.nanoTime();
-        return "Sorted Array: " + arrayToString(array) + ", Time taken: " + (endTime - startTime) + " nanoseconds";
+    public Map<String, Object> bubbleSort(@PathVariable int[] array) {
+        return sortArray(array, BubbleSort::sort);
     }
 
     @GetMapping("/sort/insertion/{array}")
-    public String insertionSort(@PathVariable int[] array) {
-        long startTime = System.nanoTime();
-        InsertionSort.sort(array);
-        long endTime = System.nanoTime();
-        return "Sorted Array: " + arrayToString(array) + ", Time taken: " + (endTime - startTime) + " nanoseconds";
+    public Map<String, Object> insertionSort(@PathVariable int[] array) {
+        return sortArray(array, InsertionSort::sort);
     }
 
     @GetMapping("/sort/merge/{array}")
-    public String mergeSort(@PathVariable int[] array) {
-        long startTime = System.nanoTime();
-        MergeSort.sort(array);
-        long endTime = System.nanoTime();
-        return "Sorted Array: " + arrayToString(array) + ", Time taken: " + (endTime - startTime) + " nanoseconds";
+    public Map<String, Object> mergeSort(@PathVariable int[] array) {
+        return sortArray(array, MergeSort::sort);
     }
 
     @GetMapping("/sort/selection/{array}")
-    public String selectionSort(@PathVariable int[] array) {
+    public Map<String, Object> selectionSort(@PathVariable int[] array) {
+        return sortArray(array, SelectionSort::sort);
+    }
+
+    private Map<String, Object> sortArray(int[] array, SortingFunction sortFunction) {
         long startTime = System.nanoTime();
-        SelectionSort.sort(array);
+        sortFunction.sort(array);
         long endTime = System.nanoTime();
-        return "Sorted Array: " + arrayToString(array) + ", Time taken: " + (endTime - startTime) + " nanoseconds";
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("sortedArray", arrayToString(array));
+        result.put("timeTaken", endTime - startTime);
+        return result;
     }
 
     private String arrayToString(int[] array) {
@@ -50,5 +53,10 @@ public class SortingController {
             sb.append(value).append(" ");
         }
         return sb.toString().trim();
+    }
+
+    @FunctionalInterface
+    interface SortingFunction {
+        void sort(int[] array);
     }
 }
